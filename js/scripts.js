aef80acef214f9000200408977b284e522afdbf2
ani <anna.unton@yahoo.com>
2017-11-11 20:41:16 +0700

    Upper and LowerCase validation added

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,22 @@ var allNames = femaleNames.concat(maleNames);
 console.log(allNames);
 
 var newName = prompt('Enter new name');
+function newNameModified(newName) {
+	
+	var newNameFirstLetter = newName[0].toUpperCase();
+	var newNameLowercase = newName.toLowerCase();
+	var newNameLowercaseCut = newNameLowercase.slice(1);
+	
+	return newNameFirstLetter + newNameLowercaseCut;
 
-var nameIndex = allNames.indexOf(newName);
+} 
+var finalName = newNameModified(newName);
 
+var nameIndex = allNames.indexOf(finalName);
 if (nameIndex == -1) {
-	allNames.push(newName);
+	allNames.push(finalName);
 	console.log(allNames);
 	} else {
-	console.log(newName +' is already in table. Select another name');
+	console.log(finalName +' is already in this table. Enter another name');
 	}
 
